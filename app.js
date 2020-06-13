07ff1cb43a8b914b278fe877966686657f757b02
Anum Hussain <anumhussain19@gmail.com>
2020-06-13 14:46:45 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,285 @@
+
+                                  // chapter 1
+// alert("TASK 1")
+// alert("Welcome to my website");
+
+// alert("TASK 2")
+
+// alert("Please enter a valid password");
+// alert("TASK 3")
+// alert("Welcome to JS Land... \n Happy coding!");
+// alert("TASK 4")
+// alert("Welcome to Js Land...");
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs ");
+
+// alert("TASK 5, open the console to see the output")
+// alert(console.log("Hello...I can run JS through my web browser's console"));
+
+
+                                    // chapter 2
+    //  task 1                               
+// var username;
+
+   //  task 2
+// var myName = "Anum Hussain";
+
+
+   //  task 3   
+// var message = "Hello World"
+// alert(message);
+
+
+   //  task 4
+//    var name ="Anum Husssain";
+//    var age ="23 years old";
+//    var course = "Certified mobile app development";
+//    alert(name);
+//    alert(age);
+//    alert(course);
+
+
+   //  task 5
+//    var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+//    alert(pizza);
+
+
+ //  task 6
+
+//  var email = "anumhussain19@gmail.com";
+//  alert("my email address is " +email);
+
+
+// task 7
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn JavaScript from the book " + book)
+
+// task 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+ //  task 9
+//  alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+
+ //                              chapter 3
+ //task 1
+//  var age= 15;
+//  alert("I am " +age +" year old");
+
+
+ //task 2
+
+ //task 3
+//  var birthYear = 1996;
+//  document.write("My birth year is" +birthYear + "<br>" + " Data type of my declared variable is number");
+ 
+ //task 4
+//  var visitorsName = "Anum";
+//  var productTiltle ="shirt";
+//  var quantity = 3;
+
+//  document.write(visitorsName + "\t ordered \t" +quantity+ "\t"+productTiltle +"\t from XYZ clothing Store.");
+
+
+//                              chapter 4
+//task 1
+// var a,b,c;
+//task 2
+//  legal variables 
+
+// var myVar, var_two, $var3, var4, varFive;
+
+ //  illegal variables
+
+// var var#@$, 12var, my variable, 123, var, 
+//task 3
+// document.write("<h1> Rules for naming JS variables </h1> <br> Variable names can only contain numbers, $ and _. For example <b>$my_1stVariable</b> <br>");
+// document.write("Variables must begin with a letter,$ or _. For example $name, _name or name <br>");
+// document.write("Variable names are case sensitive <br>");
+// document.write("Variable names should not be JS keywords");
+
+//                              chapter 5
+
+//task 1
+// var num1 = 3;
+// var num2 = 5;
+// var c = num1+num2;
+// document.write("the sum of " +num1 +" and " +num2 +" is " +c);
+
+
+//task 2
+//subtraction
+// var num1 = 5;
+// var num2 = 3;
+// var c = num1-num2;
+// document.write("the subtraction of " +num1 +" and " +num2 +" is " +c);
+
+//multiplication
+// var num1 = 5;
+// var num2 = 3;
+// var c = num1*num2;
+// document.write("the multiplication of " +num1 +" and " +num2 +" is " +c);
+
+//division
+
+// var num1 = 15;
+// var num2 = 5;
+// var c = num1/num2;
+// document.write("the division of " +num1 +" and " +num2 +" is " +c);
+
+
+//modulus
+
+// var num1 = 5;
+// var num2 = 3;
+// var c = num1%num2;
+// document.write("the remainder of " +num1 +" and " +num2 +" is " +c);
+
+//task 3
+// var myVar;
+// document.write("Value after variable declaration is " +myVar);
+// myVar= 9;
+// document.write("<br> Initial value: " + myVar);
+// myVar++;
+// document.write("<br> Value after increment is: " +myVar);
+// myVar = myVar+7;
+// document.write("<br> Value after addition is:" +myVar);
+// myVar--;
+// document.write(" <br> Value after decrement is: " +myVar);
+// myVar= myVar%3;
+// document.write("<br> the remainder is: " +myVar);
+
+//task 4
+// var ticket = 600;
+// var noOfTickets = 5;
+// document.write("total cost to buy "+noOfTickets +" tickets to a movie is " +ticket*noOfTickets +" PKR");
+
+// task 5
+// var num = prompt("Enter any number");
+// var i;
+// document.write(num);
+
+// if(num!=0 || null){
+//    document.write("<h1>Table of " +num +"<br> </h1>");
+// for(i=1; i<11; i++)
+// {
+//    document.write(num +" *" +i +" =" + num*i +"<br>" );
+   
+// }}
+// else{
+//    document.write("<h1>Table of 5 <br> </h1>");
+//    for(i=1; i<11; i++)
+// {
+ 
+//    document.write(5 +" *" +i +" =" +5*i +"<br>" );
+   
+// }
+
+// }
+
+//task 6
+
+// var celsius = prompt("Enter a temperature in celsius ");
+// var fahrenheit = prompt("Enter a temperature in fahrenheit");
+
+
+// document.write(+celsius +" celsius is " + ((+celsius*9/5)+32) +"<br>" );
+
+
+// document.write(+fahrenheit +" fahrenhiet is " + ((+fahrenheit-32)*5/9) );
+
+//task 7
+
+
+// var pItem1 = 650;
+// var pItem2 = 100;
+// var qty1 =3;
+// var qty2 = 7;
+// var shippingCharges = 100;
+
+// document.write("Price of item 1 is " +pItem1 +"<br>");
+// document.write("Quantity of item 1 is " +qty1 +"<br>");
+
+
+// document.write("Price of item 2 is " +pItem2 +"<br>");
+// document.write("Quantity of item 2 is " +qty2 +"<br>");
+// document.write("Shipping charges " +shippingCharges +"<br>");
+// document.write("Total cost of your order is " +(pItem1*qty1 + pItem2*qty2+shippingCharges));
+
+
+//task 8
+
+// var totalMarks = 980;
+// var marksObt = 804;
+// document.write("Total Marks: " +totalMarks +"<br> Marks Obtsained: " +marksObt +"<br> Percentage: " +(marksObt/totalMarks)*100);
+
+//task 9
+
+// var USdollar = 10;
+// var riyal = 25;
+// document.write("Total currency in PKR " +(USdollar*104.80 + riyal*28) );
+
+//task 10
+// var num = parseInt(prompt("Enter any number"));
+
+
+// document.write(((num+5)*10)/2);
+
+//task 11
+
+// var currentYear = 2020;
+// var birthYear = 1996;
+// var age = currentYear-birthYear;
+// document.write("Current year: " +currentYear +"<br> Birth Year: " +birthYear +"<br> Your age is: " +age);
+
+//task 12
+// var radius = 6;
+// var pi = 3.142;
+
+// document.write("The circumference of a cirle is: " +2*pi*radius);
+// document.write("<br> Area of a circle is: " +pi*radius*radius);
+
+//task 13
+// var snacks = "Lays";
+// var age = 24;
+// var maxAge = 30;
+// var amount = 2;
+// document.write("You will need " + (maxAge-age)*amount +"\t"+ snacks +" to last you until the ripe old age of " +maxAge);
+
+                              //chapter 6-9
+//task 1
+// var a = 10;
+// document.write("the value is:" +a);
+// a= ++a;
+// document.write("<br> the value after ++a is:" +a);
+// document.write("<br> now the value of a is: " +a);
+
+// a=a++;
+
+// document.write("<br> the value of a++ is: " +a);
+// document.write("<br> now the value of a is: " +a++);
+// a=--a;
+// document.write("<br> the value of --a is: " +a );
+// document.write("<br> now the value of a is: " +a);
+
+// a=a--;
+// document.write("<br> the value of a-- is: " +a );
+// document.write("<br> now the value of a is: " +a);
+
+//task 2
+
+// var a=2, b=1;
+// var result = --a - --b + ++b + b--;
+// document.write(result);
+// --a= 1;
+// --b=0;
+// --a - --b = 1
+// --a - --b + ++b= 2;
+// --a - --b + ++b + b--= 3;
+
+//task 3
+// var name = prompt("enter your name");
+// alert("Welcome " +name);
+
+//task 3
+//alreay done above
